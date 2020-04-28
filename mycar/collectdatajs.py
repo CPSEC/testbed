b16c0dbecbe3d2c2199ea58888f66075ea8b4482
lin <20693521+lion-zhang@users.noreply.github.com>
2020-04-27 23:41:07 -0400

    optimize the control loop

--- mycar/collectdatajs.py
+++ mycar/collectdatajs.py
@@ -23,14 +23,14 @@ hcsr04 = HCSR04()
 V.add(hcsr04, outputs=['hcsr04'], threaded=True)
 
 ads1115 = ADS1115(coeff_m=cfg.VM_COEFFICIENT, coeff_p=cfg.VP_COEFFICIENT)
-V.add(ads1115, outputs=['ads1115/vm', 'ads1115/vp'])
+V.add(ads1115, outputs=['ads1115/vm', 'ads1115/vp'], threaded=True)
 
 bno055 = BNO055()
 V.add(bno055, outputs=["bno055/heading", "bno055/roll", "bno055/pitch", "bno055/sys", "bno055/gyro", "bno055/accel",
                        "bno055/mag", "bno055/ori_x", "bno055/ori_y", "bno055/ori_z", "bno055/ori_w", "bno055/temp_c",
                        "bno055/mag_x", "bno055/mag_y", "bno055/mag_z", "bno055/gyr_x", "bno055/gyr_y", "bno055/gyr_z",
                        "bno055/acc_x", "bno055/acc_y", "bno055/acc_z", "bno055/lacc_x", "bno055/lacc_y",
-                       "bno055/lacc_z", "bno055/gra_x", "bno055/gra_y", "bno055/gra_z"])
+                       "bno055/lacc_z", "bno055/gra_x", "bno055/gra_y", "bno055/gra_z"], threaded=True)
 
 as5048a = AS5048A()
 V.add(as5048a, outputs=['as5048a'], threaded=True)
@@ -63,7 +63,7 @@ V.add(steering, inputs=['user/angle'])
 V.add(throttle, inputs=['user/throttle'])
 
 oled = OLED(OLED_KEY=cfg.OLED_KEY, OLED_PORT=cfg.OLED_PORT)
-V.add(oled, inputs=['recording'])
+V.add(oled, inputs=['recording'], threaded=True)
 
 # data save
 inputs = ['hcsr04', 'ads1115/vm', 'ads1115/vp', 'as5048a', "bno055/heading", "bno055/roll", "bno055/pitch",
@@ -85,7 +85,7 @@ types = ['float', 'float', 'float', 'int', 'float', 'float', 'float',
 
 # one csv file
 writer = CSVDATA(path=cfg.DATA_PATH, inputs=inputs)
-V.add(writer, inputs=inputs, run_condition='recording')
+V.add(writer, inputs=inputs, threaded=True, run_condition='recording')
 
 
 # run the vehicle for 20 seconds
