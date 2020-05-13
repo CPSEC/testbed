f552246e27041cb834cad20ce3587da1c695b17b
Lion <20693521+lion-zhang@users.noreply.github.com>
2020-05-13 17:08:34 -0400

    increase interval

--- mycar/motor_estimate.py
+++ mycar/motor_estimate.py
@@ -14,7 +14,7 @@ cfg = dk.load_config()
 meta = []
 V = Vehicle()
 
-pulse = Pulse(interval=3, cycle=0.5, length=20, min=0.0, max=0.2)
+pulse = Pulse(interval=4, cycle=0.5, length=20, min=0.0, max=0.2)
 V.add(pulse, outputs=['user/throttle', 'recording'])
 
 
