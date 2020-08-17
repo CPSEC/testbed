b68c9926cece55f75c8cd28ffe74bb3f003f2809
lion-zhang <20693521+lion-zhang@users.noreply.github.com>
2020-08-16 22:02:00 -0400

    add myconfig

--- mycar/config.py
+++ mycar/config.py
@@ -231,18 +231,4 @@ PID_THROTTLE = 0.2                  # constant throttle value during path follow
 SAVE_PATH_BTN = "cross"             # joystick button to save path
 RESET_ORIGIN_BTN = "triangle"       # joystick button to press to move car back to origin
 
-# NEW
-OLED_KEY = '7jogUVtgh5v@h^'
-OLED_PORT = 13202
-VM_COEFFICIENT = 0.000359
-VP_COEFFICIENT = 0.000354
 
-host = '192.168.0.11'
-port = 13244
-sep = '\x1e'
-Motor_P = 0.001748
-Motor_I = 0
-Motor_D = 0
-Servo_P = 0.01
-Servo_I = 0
-Servo_D = 0
