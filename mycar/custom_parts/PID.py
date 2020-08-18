3d88b5dc08f482e20e6b43ffedd240bf5479d929
lion-zhang <20693521+lion-zhang@users.noreply.github.com>
2020-08-18 14:42:37 -0400

    new PID

--- mycar/custom_parts/PID.py
+++ mycar/custom_parts/PID.py
@@ -1,164 +1,67 @@
 import time
-import logging
-import joblib
-import datetime
 
 
-# How to call this part?
-# V = Vehicle()
-# t = template(p1)
-# V.add(t, input=['in1', 'in2'], output=['tdata1','tdata2'], threaded=True)
-# V.start()
-
-class PIDSpeed:
-    def __init__(self, pid):
-        self.pid = pid
+class PID:
+    def __init__(self, p, i, d, dt, control_up, control_lo):
         self.on = True
-        # initiate your data
-        self.throttle = 0
-        self.recording = True
-
-        # currentTime = datetime.datetime.now()
-        # currentLength = 5
-        # length = 30
-        # self.nextTime = currentTime + datetime.timedelta(seconds=currentLength)
-        # self.endTime = currentTime + datetime.timedelta(seconds=length)
-
-    def run(self, p, i, d, cse):
+        # optional, parameter p1
         self.p = p
         self.i = i
         self.d = d
-        self.cse = cse
-        self.poll()
-        return self.throttle,self.recording
+        self.dt = dt
+        self.c_up = control_up
+        self.c_lo = control_lo
+        # initiate your data
+        self.int_error = 0
+        self.pre_error = 0
+        # Initiate your part here
 
-        # Call in the control loop
-        # Works when threaded=False
-        # Input is parameters, Return your output
+    def run(self, set_point, measurement, p=None, i=None, d=None):
+        # needed when turning
+        if p:
+            self.p = p
+        if i:
+            self.i = i
+        if d:
+            self.d = d
+
+        # error
+        error = set_point - measurement
+
+        # integration
+        self.int_error += error * self.dt
+        iterm = self.int_error * self.i
+        # integration anti-windup
+        iterm = self.clamp(iterm)
+
+        # differentiation
+        diff = (error - self.pre_error) / self.dt
+        dterm = diff * self.d
+
+        # proportion
+        pterm = error * self.p
+
+        control = pterm + iterm + dterm
+        control = self.clamp(control)
+
+        # save state
+        self.pre_error = error
+
+        return control
+
+    def clamp(self, value):
+        if value < self.c_lo:
+            return self.c_lo
+        if value > self.c_up:
+            return self.c_up
+        return value
 
     def shutdown(self):
         self.on = False
         time.sleep(0.2)
         # Call once before stop the part
 
-    def update(self):
-        while self.on:
-            self.poll()
-            time.sleep(self.poll_delay)
-        # your thread
-        # Works when threaded=True
 
-    def run_threaded(self, in1, in2):
-        pass
-        # return self.tdata1, self.tdata2
-        # Call in the control loop
-        # Works when threaded=True
-        # Similar as run function
-
-
-    def poll(self):
-        # currentTime = datetime.datetime.now()
-        # if currentTime > self.endTime:
-        #     self.data = 0
-        #     self.recording = False
-        #     self.on = False
-        #     return
-        # if currentTime > self.nextTime:
-        self.throttle = self.pid.run(self.p, self.i, self.d, self.cse)
-        logging.info("CurrentSpeedError: %f Speed: %f" % (self.cse, self.throttle))
-
-        # print(self.throttle)
-
-        # your actual function of the thread
-
-
-class PIDController:
-    """ Performs a PID computation and returns a control value.
-        This is based on the elapsed time (dt) and the current value of the process variable
-        (i.e. the thing we're measuring and trying to change).
-        https://github.com/chrisspen/pid_controller/blob/master/pid_controller/pid.py
-    """
-
-    def __init__(self, debug=False):
-
-        # initialize gains
-        # self.Kp = p
-        # self.Ki = i
-        # self.Kd = d
-
-        # The value the controller is trying to get the system to achieve.
-
-
-        # initialize delta t variables
-        self.prev_tm = time.time()
-        self.prev_err = 0
-        self.error = None
-        self.totalError = 0
-
-        # initialize the output
-        self.alpha = 0
-
-        # debug flag (set to True for console output)
-        self.debug = debug
-
-    def run(self, p, i, d, err):
-        self.Kp = p
-        self.Ki = i
-        self.Kd = d
-        curr_tm = time.time()
-
-        self.difError = err - self.prev_err
-
-        # Calculate time differential.
-        dt = curr_tm - self.prev_tm
-
-        # Initialize output variable.
-        curr_alpha = 0
-
-        # Add proportional component.
-        curr_alpha += self.Kp * err
-
-        # Add integral component.
-        curr_alpha += self.Ki * (self.totalError * dt)
-
-        # Add differential component (avoiding divide-by-zero).
-        if dt > 0:
-            curr_alpha += self.Kd * ((self.difError) / float(dt))
-
-        # Maintain memory for next loop.
-        self.prev_tm = curr_tm
-        self.prev_err = err
-        self.totalError += err
-
-        # Update the output
-        self.alpha = curr_alpha
-
-        if (self.debug):
-            print('PID err value:', round(err, 4))
-            print('PID output:', round(curr_alpha, 4))
-
-        return curr_alpha
-
-
-class CSE(object):
-    def __init__(self):
-        #self.target = target
-        pass
-
-    def run(self, target, speed):
-        self.target = target
-        cse = float(self.target) - speed
-        # print(speed)
-
-        return cse
-
-
-# # test
-# if __name__ == "__main__":
-#     iter = 0
-#     t = RPulse()
-#     while iter < 80:
-#         data, recording = t.run()
-#         print(data, ' ', recording)
-#         iter += 1
-#         time.sleep(0.5)
+# test
+if __name__ == "__main__":
+    pass
--- mycar/motor_pid_tuning.py
+++ mycar/motor_pid_tuning.py
@@ -1,24 +1,51 @@
 from donkeycar.vehicle import Vehicle
 import donkeycar as dk
+from donkeycar.parts.actuator import PCA9685, PWMSteering, PWMThrottle
 from custom_parts.AS5048A_Process import speed
 from custom_parts.SocketData import SocketData
 from custom_parts.Clock import Clock
+from custom_parts.PID import PID
+from custom_parts.pulse_generator import Pulse
 
 cfg = dk.load_config()
 V = Vehicle()
 
-sensor = ['as5048a']
+sensor = ['as5048a', 'throttle']
 parameter = ['rspeed', 'mp', 'mi', 'md', 'sp', 'si', 'sd']
 setting = ['rspeed', 'mp', 'mi', 'md', 'sp', 'si', 'sd']
 V.mem.put(parameter, [40, cfg.MOTOR_P, cfg.MOTOR_I, cfg.MOTOR_D, cfg.SERVO_P, cfg.SERVO_I, cfg.SERVO_D])
 
+# time of current control step
 clock = Clock()
 V.add(clock, outputs=['milliseconds'])
 
+# speed sensor
 as5048a = speed()
 V.add(as5048a, outputs=['as5048a'])
 
-# def __init__(self, host, port, sensor, parameter, setting, sep, image=False, poll_delay=0.01):
+# reference speed (set point)
+pulse = Pulse(interval=10, cycle=0.5, length=100, min=30, max=60)
+V.add(pulse, outputs=['rspeed', 'recording'])
+
+# pid controller
+p = cfg.MOTOR_P
+i = cfg.MOTOR_I
+d = cfg.MOTOR_D
+dt = 1/cfg.DRIVE_LOOP_HZ
+control_up = 1
+control_lo = 0
+motor_pid = PID(p, i, d, dt, control_up, control_lo)
+V.add(motor_pid, inputs=['rspeed', 'as5048a', 'mp', 'mi', 'md'], outputs=['throttle'])
+
+# actuator - Motor
+throttle_controller = PCA9685(cfg.THROTTLE_CHANNEL, cfg.PCA9685_I2C_ADDR, busnum=cfg.PCA9685_I2C_BUSNUM)
+throttle = PWMThrottle(controller=throttle_controller,
+                       max_pulse=cfg.THROTTLE_FORWARD_PWM,
+                       zero_pulse=cfg.THROTTLE_STOPPED_PWM,
+                       min_pulse=cfg.THROTTLE_REVERSE_PWM)
+V.add(throttle, inputs=['throttle'])
+
+# send data
 host = cfg.HOST
 port = cfg.PORT
 sep = cfg.SEP
