68d4f41409e75c0c05bbc8578fa9c762f5ab7ab3
lion-zhang <20693521+lion-zhang@users.noreply.github.com>
2020-09-07 02:16:21 -0400

    as5048a rewrite

--- mycar/custom_parts/AS5048A.py
+++ mycar/custom_parts/AS5048A.py
@@ -1,117 +1,96 @@
-import board
-import busio
-import digitalio
-import time
+# requirements:
+# spi-dev
+import spidev
 
-CMD_ANGLE = bytearray([0xff, 0xff])
-CMD_AGC = bytearray([0x7f, 0xfd])
-CMD_MAG = bytearray([0x7f, 0xfe])
-CMD_CLAER = bytearray([0x42, 0x01])
-CMD_NOP = bytearray([0xc0, 0x00])
+CMD_READ = 0X4000
+CMD_ANGLE = 0x3FFF
+CMD_READ_MAG = 0x3FFE
+CMD_READ_DIAG = 0x3FFD
+CMD_NOP = 0x0000
+CMD_CLEAR_ERROR = 0x0001
+CMD_ProgramControl = 0x0003
+CMD_OTPHigh = 0x0016
+CMD_OTPLow = 0x0017
 
 
 class AS5048A:
+    """
+    class for sensor AS5048A (SPI protocol)
+    default /dev/spidev0.0
+      signal      physical pin
+     SPI0_MOSI         19
+     SPI0_MISO         21
+     SPI0_SCLK         23
+     SPI0_CE0          24
 
-    def __init__(self, poll_delay=0.000001):
-        self.spi = busio.SPI(board.SCK, MISO=board.MISO)
-        self.cs = digitalio.DigitalInOut(board.CE0)
-        self.cs.direction = digitalio.Direction.OUTPUT
-        self.cs.value = True
-        while not self.spi.try_lock():
-            pass
-        self.spi.configure(baudrate=3000000, polarity=0, phase=1)
-        self.spi.unlock()
+    :param bus: spi bus number
+    :param device: spi chip-select pin to use
+    :param speed: spi bus speed in Hz - Max 10MHz
+    """
 
-        # data
-        self.angle = 0
-        self.sampletime = 0
-        self.last_angle = 0
-        self.last_sampletime = 0
-        self.sum_angle = 0
-        self.sum_time = 0
+    def __init__(self, bus=0, device=0, speed=7800000):
+        self.spi = spidev.SpiDev()
+        self.spi.open(bus, device)  # default /dev/spidev0.0
+        self.spi.max_speed_hz = speed  # Max 10MHz
+        self.spi.mode = 1  # polarity=0, phase=1
 
-        self.on = True
-        self.poll_delay = poll_delay
+    @staticmethod
+    def calc_parity(v):
+        """
+        Args:
+            v: data - int 16bit
+        Returns: 1 (odd) or 0 (even)
+        """
+        if v == 0:
+            return 0
+        v ^= v >> 8
+        v ^= v >> 4
+        v ^= v >> 2
+        v ^= v >> 1
+        return v & 1
 
-    def update(self):
-        while self.on:
-            self.poll()
-            time.sleep(self.poll_delay)
+    def read_reg(self, cmd):
+        """
+        Args:
+            cmd: AS5048A command
+        Returns: data or -1 (error)
+        """
+        command = CMD_READ | cmd
+        command |= (self.calc_parity(command) << 15)
+        self.spi.xfer([command >> 8 & 0xff, command & 0xff])
 
-    def spi_write_read(self, cmd):
-        result = bytearray(2)
-        self.cs.value = False
-        self.spi.write_readinto(cmd, result)
-        self.cs.value = True
-        time.sleep(350 / 1000000000)
-        return result
+        command = CMD_READ | CMD_NOP
+        command |= (self.calc_parity(command) << 15)
+        res = self.spi.xfer([command >> 8 & 0xff, command & 0xff])
+
+        error_flag = 1
+        data = 0
+        if res[0] & (1 << 6):
+            data = (res[0] & 0x3f) << 8 + res[1]
+            error_flag = self.calc_parity(data) ^ (res[0] >> 7)
+            if error_flag:
+                print("AS5048A: Parity bit check failed.")
+        else:  # error
+            print("AS5048A: There exists a transmission error in a previous host transmission.")
+            command = CMD_READ | CMD_CLEAR_ERROR
+            command |= (self.calc_parity(command) << 15)
+            self.spi.xfer([command >> 8 & 0xff, command & 0xff])
+
+        if error_flag:
+            return -1
+        return data
 
     def get_angle(self):
-        while not self.spi.try_lock():
-            pass
-        try:
-            self.spi_write_read(CMD_ANGLE)
-            result = self.spi_write_read(CMD_NOP)
-            result0 = result[0] & 0x3f
-            result1 = result[1] & 0xf0  # ignore the last four value
-            self.angle = (result0 << 8) + result1  # 0-0x3fff
-            self.sampletime = time.time_ns()
-            # clear error flag
-            if result[0] & 0x40 > 0:
-                print('error flag clear')
-                self.spi_write_read(CMD_CLAER)
-        finally:
-            self.spi.unlock()
+        return self.read_reg(CMD_ANGLE)
 
-    def poll(self):
-        self.last_angle = self.angle
-        self.last_sampletime = self.sampletime
-        self.get_angle()
-        if self.angle == 0:
-            # did not get value, pass this poll
-            self.angle = self.last_angle
-            self.sampletime = self.last_sampletime
-        else:
-            theta_angle = self.last_angle - self.angle
-            # go through zero  (0x3fff=16383)
-            if theta_angle < -10467:
-                theta_angle += 0x3fff
-            if theta_angle > 10467:
-                theta_angle -= 0x3fff
-            self.sum_angle += theta_angle
-            theta_time = self.sampletime - self.last_sampletime
-            self.sum_time += theta_time
-
-    def run_threaded(self):
-        r = self.sum_angle / 0x3fff
-        s = self.sum_time / 1000000000
-        if s == 0:
-            s = 0.01  # impossible, just in case
-        self.sum_angle = 0
-        self.sum_time = 0
-        return r / s
-
-    def run(self):
-        # do not expect use threaded=False
-        pass
-
-    def shutdown(self):
-        self.on = False
+    def close(self):
+        self.spi.close()
 
 
-if __name__ == "__main__":
-    itr = 0
-    lst = []
+if "__name__" == "__main__":
     as5048a = AS5048A()
-    while itr < 5000:
-        itr += 1
-        as5048a.poll()
-        time.sleep(0.001)
-        if itr % 20 == 0:
-            speed = as5048a.run_threaded()
-            lst.append(speed)
-    print(lst)
-    # while itr < 500:
-    #     as5048a.run()
-    #     print(as5048a.angle)
-    #     itr += 1
+    for i in range(10):
+        print(hex(as5048a.get_angle()))
+
+
+
---
+++ mycar/custom_parts/AS5048A_old.py
@@ -0,0 +1,117 @@
+import board
+import busio
+import digitalio
+import time
+
+CMD_ANGLE = bytearray([0xff, 0xff])
+CMD_AGC = bytearray([0x7f, 0xfd])
+CMD_MAG = bytearray([0x7f, 0xfe])
+CMD_CLAER = bytearray([0x42, 0x01])
+CMD_NOP = bytearray([0xc0, 0x00])
+
+
+class AS5048A:
+
+    def __init__(self, poll_delay=0.000001):
+        self.spi = busio.SPI(board.SCK, MISO=board.MISO)
+        self.cs = digitalio.DigitalInOut(board.CE0)
+        self.cs.direction = digitalio.Direction.OUTPUT
+        self.cs.value = True
+        while not self.spi.try_lock():
+            pass
+        self.spi.configure(baudrate=3000000, polarity=0, phase=1)
+        self.spi.unlock()
+
+        # data
+        self.angle = 0
+        self.sampletime = 0
+        self.last_angle = 0
+        self.last_sampletime = 0
+        self.sum_angle = 0
+        self.sum_time = 0
+
+        self.on = True
+        self.poll_delay = poll_delay
+
+    def update(self):
+        while self.on:
+            self.poll()
+            time.sleep(self.poll_delay)
+
+    def spi_write_read(self, cmd):
+        result = bytearray(2)
+        self.cs.value = False
+        self.spi.write_readinto(cmd, result)
+        self.cs.value = True
+        time.sleep(350 / 1000000000)
+        return result
+
+    def get_angle(self):
+        while not self.spi.try_lock():
+            pass
+        try:
+            self.spi_write_read(CMD_ANGLE)
+            result = self.spi_write_read(CMD_NOP)
+            result0 = result[0] & 0x3f
+            result1 = result[1] & 0xf0  # ignore the last four value
+            self.angle = (result0 << 8) + result1  # 0-0x3fff
+            self.sampletime = time.time_ns()
+            # clear error flag
+            if result[0] & 0x40 > 0:
+                print('error flag clear')
+                self.spi_write_read(CMD_CLAER)
+        finally:
+            self.spi.unlock()
+
+    def poll(self):
+        self.last_angle = self.angle
+        self.last_sampletime = self.sampletime
+        self.get_angle()
+        if self.angle == 0:
+            # did not get value, pass this poll
+            self.angle = self.last_angle
+            self.sampletime = self.last_sampletime
+        else:
+            theta_angle = self.last_angle - self.angle
+            # go through zero  (0x3fff=16383)
+            if theta_angle < -10467:
+                theta_angle += 0x3fff
+            if theta_angle > 10467:
+                theta_angle -= 0x3fff
+            self.sum_angle += theta_angle
+            theta_time = self.sampletime - self.last_sampletime
+            self.sum_time += theta_time
+
+    def run_threaded(self):
+        r = self.sum_angle / 0x3fff
+        s = self.sum_time / 1000000000
+        if s == 0:
+            s = 0.01  # impossible, just in case
+        self.sum_angle = 0
+        self.sum_time = 0
+        return r / s
+
+    def run(self):
+        # do not expect use threaded=False
+        pass
+
+    def shutdown(self):
+        self.on = False
+
+
+if __name__ == "__main__":
+    itr = 0
+    lst = []
+    as5048a = AS5048A()
+    while itr < 5000:
+        itr += 1
+        as5048a.poll()
+        time.sleep(0.001)
+        if itr % 20 == 0:
+            speed = as5048a.run_threaded()
+            lst.append(speed)
+    print(lst)
+    # while itr < 500:
+    #     as5048a.run()
+    #     print(as5048a.angle)
+    #     itr += 1
