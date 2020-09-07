2253bd120491cbdd94505332534c1d6a73f23a68
lion-zhang <20693521+lion-zhang@users.noreply.github.com>
2020-09-07 10:55:30 -0400

    add debug info

--- mycar/custom_parts/AS5048A.py
+++ mycar/custom_parts/AS5048A.py
@@ -49,7 +49,7 @@ class AS5048A:
         v ^= v >> 1
         return v & 1
 
-    def read_reg(self, cmd):
+    def read_reg(self, cmd, debug=True):
         """
         Args:
             cmd: AS5048A command
@@ -57,21 +57,29 @@ class AS5048A:
         """
         command = CMD_READ | cmd
         command |= (self.calc_parity(command) << 15)
-        self.spi.xfer([command >> 8 & 0xff, command & 0xff])
+        cmd_lst = [command >> 8 & 0xff, command & 0xff]
+        self.spi.xfer(cmd_lst)
+        if debug:
+            print('sent', [hex(value) for value in cmd_lst])
 
         command = CMD_READ | CMD_NOP
         command |= (self.calc_parity(command) << 15)
-        res = self.spi.xfer([command >> 8 & 0xff, command & 0xff])
+        cmd_lst = [command >> 8 & 0xff, command & 0xff]
+        res = self.spi.xfer(cmd_lst)
+        if debug:
+            print('sent:', [hex(value) for value in cmd_lst])
+            print('received:', [hex(value) for value in res])
 
         error_flag = 1
         data = 0
         if res[0] & (1 << 6):
             data = (res[0] & 0x3f) << 8 + res[1]
             error_flag = self.calc_parity(data) ^ (res[0] >> 7)
-            if error_flag:
+            if error_flag & debug:
                 print("AS5048A: Parity bit check failed.")
         else:  # error
-            print("AS5048A: There exists a transmission error in a previous host transmission.")
+            if debug:
+                print("AS5048A: There exists a transmission error in a previous host transmission.")
             command = CMD_READ | CMD_CLEAR_ERROR
             command |= (self.calc_parity(command) << 15)
             self.spi.xfer([command >> 8 & 0xff, command & 0xff])
@@ -80,8 +88,8 @@ class AS5048A:
             return -1
         return data
 
-    def get_angle(self):
-        return self.read_reg(CMD_ANGLE)
+    def get_angle(self, debug=True):
+        return self.read_reg(CMD_ANGLE, debug=debug)
 
     def close(self):
         self.spi.close()
