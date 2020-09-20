3a4f51a6deaf16e99e44575428845f73984e7ded
lion-zhang <20693521+lion-zhang@users.noreply.github.com>
2020-09-20 18:38:23 -0400

    test hc-sr04

---
+++ TEST_CODE/HC-SR04.py
@@ -0,0 +1,18 @@
+# wiring
+# Trig  ----  GPIO27
+# Echo  ----  GPIO17
+
+# pip install adafruit-circuitpython-hcsr04
+
+import time
+import board
+import adafruit_hcsr04
+
+sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D27, echo_pin=board.D17)
+
+while True:
+    try:
+        print((sonar.distance,))
+    except RuntimeError:
+        print("Retrying!")
+    time.sleep(0.1)
